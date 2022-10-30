6bff399ff2680c15dc39d864f1e554ee4dd17f42
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-10-30 13:25:42 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,21 +2,21 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
 player.on('timeupdate', function(data) {
-        localStorage.setItem("videoplayer-current-time", data.seconds)
+    localStorage.setItem("videoplayer-current-time", data.seconds)
     const currentTime = localStorage.getItem('videoplayer-current-time')
-    return currentTime
+    console.log(currentTime);
 });
-console.log(currentTime);
-    player.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+//     player.setCurrentTime(currentTime).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
