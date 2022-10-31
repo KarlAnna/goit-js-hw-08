5c61722419cde8c61808ab9c39f0841490633a14
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-10-31 14:39:11 +0200

    complete 1,3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,37 @@
+import VimeoPlayer from "@vimeo/player"
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new VimeoPlayer(iframe);
 
 player.on('timeupdate', function (data) {
-    console.log(data.seconds);
-    // localStorage.setItem("videoplayer-current-time", data.seconds)
-    // const currentTime = localStorage.getItem('videoplayer-current-time')
-    // console.log(currentTime);
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+    const currentTime = localStorage.getItem('videoplayer-current-time')
+    console.log(player);
+
+    player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
 });
 
-//     player.setCurrentTime(currentTime).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
+// updateTime()  {
+//         console.log(player);
+//        this.currentTime = this.player.getCurrentTime().then(function(seconds) {
+//             console.log('time updated! ' + seconds + "  !!! " );
+//             localStorage.setItem("videoplayer-current-time", seconds);
+//         }).catch(function(error) {
+//         // an error occurred
+//         });
+//        this.player.on('timeupdate', throttle(this.updateTime, 1000));
+//     };
+// let iplayer = new VideoPlayer();
+// iplayer.updateTime();
 
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const textarea = document.querySelector('textarea')
+const btn = document.querySelector('button')
 
+
+form.addEventListener('input', onFormInput)
+updateInput()
+btn.addEventListener('submit', onBtnSubmit)
+
+
+//* 1. Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні 
+//*    значення полів форми.Нехай ключем для сховища буде рядок "feedback-form-state".
+function onFormInput() {
+    localStorage.setItem("feedback-form-state",
+        JSON.stringify({ email: input.value, msg: textarea.value }))
+}
+
+//* 2. Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку 
+//*    поля повинні бути порожніми.
+function updateInput() {
+    const data = localStorage.getItem("feedback-form-state")
+    if (data) {
+        const parsedData = JSON.parse(data)
+        input.value = parsedData.email
+        textarea.value = parsedData.msg
+    }
+}
+
+//* 3. Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+function onBtnSubmit(e) {
+    e.preventDefault();
+    localStorage.removeItem("feedback-form-state")
+    e.currentTarget.reset()
+    const myFormData = new FormData(e.target)
+    const formDataObj = {}
+    myFormData.forEach((value, key) => (formDataObj[key] = value))
+    console.log(formDataObj)
+}
