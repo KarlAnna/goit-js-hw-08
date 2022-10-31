f0b9f67790cc6ae8048f965910d79df01a1d3082
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2022-10-31 12:39:42 +0000

    Deploying to gh-pages from @ KarlAnna/goit-js-hw-08@5c61722419cde8c61808ab9c39f0841490633a14 🚀

--- 02-video.3aa1fd82.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");new Vimeo.Player(e).on("timeupdate",(function(e){console.log(e.seconds)}))}();
-//# sourceMappingURL=02-video.3aa1fd82.js.map
--- 02-video.9815f574.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");new Vimeo.Player(e).on("timeupdate",(function(e){console.log(e.seconds)}));
-//# sourceMappingURL=02-video.9815f574.js.map
